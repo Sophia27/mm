4aefed87c9b77d491aaab437da491dea0d7bbd43
“Sophia <info@sophiagoedecke.de>
2022-07-27 20:53:39 +0200

    update6

--- title.js
+++ title.js
@@ -12,3 +12,43 @@ document.addEventListener("scroll", function () {
     }
   })
 })
+
+
+const addMovement = function () {
+  const topViewport = window.pageYOffset
+  const midViewport = topViewport + (window.innerHeight / 2)
+  
+  // lets find the middle of each section
+  // (section, index) => {}
+  sections.forEach((section, index) => {
+    const topSection = section.offsetTop
+    const midSection = topSection + (section.offsetHeight / 2)
+    
+    // how far away is the section from the visible area of the page
+    const distanceToSection = midViewport - midSection
+    
+    // pick the tags to parallax
+    const image = section.querySelector("img")
+    
+    
+    
+    let rotation = distanceToSection / 100
+    let contentDist = -1 * distanceToSection / 2
+    
+    
+  
+    image.style.transform = `rotate(${rotation}deg)`
+    
+    
+  })
+}
+
+addMovement()
+
+document.addEventListener("scroll", function () {
+  addMovement()
+})
+
+window.addEventListener("resize", function () {
+  addMovement()
+})

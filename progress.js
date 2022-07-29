337a0439781b5ec8c0342fdca3e0583b7562d6a8
“Sophia <info@sophiagoedecke.de>
2022-07-29 13:55:00 +0200

    version2

---
+++ progress.js
@@ -0,0 +1,11 @@
+const progressTag = document.querySelector ("div.progress")
+const bodyTag = document.querySelector ("body")
+
+document.addEventListener("scroll", function () {
+ const pixels = window.pageYOffset
+ const pageHeight = bodyTag.getBoundingClientRect().height
+ const totalScrollableDistance = pageHeight - window.innerHeight
+ const percentage = pixels / totalScrollableDistance
+ 
+ progressTag.style.width = `${100 * percentage}%`
+})
